91e8b10db62a237f693f8d73870fe1946b9e759f
Ícaro Chagas <icaro.chagas.a@gmail.com>
2021-07-06 04:29:10 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/educandoweb/cursomc/services/CategoriaService.java
+++ src/main/java/com/educandoweb/cursomc/services/CategoriaService.java
@@ -8,10 +8,10 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.cursomc.domain.Categoria;
+import com.educandoweb.cursomc.dto.CategoriaDTO;
 import com.educandoweb.cursomc.repositories.CategoriaRepository;
 import com.educandoweb.cursomc.services.exceptions.DataIntegrityException;
 import com.educandoweb.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,5 +58,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }

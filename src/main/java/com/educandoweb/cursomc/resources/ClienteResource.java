23056407d2be0139f6d3026b9c2f77fedab29699
Ícaro Chagas <icaro.chagas.a@gmail.com>
2021-07-05 21:19:31 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/educandoweb/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.educandoweb.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.cursomc.domain.Cliente;
+import com.educandoweb.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) { // @PathVariable permite que o Spring saiba que o id provindo da url seja mapeado para a variável id 
+		// ResponseEntity encapsula, ou armazena, várias informações de uma resposta HTTP para um serviço REST.
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}

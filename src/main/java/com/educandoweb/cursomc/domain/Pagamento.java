216af32e80cc77afd6da00e8bae0a0fc80a3333e
Ícaro Chagas <icaro.chagas.a@gmail.com>
2021-07-06 00:08:10 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/educandoweb/cursomc/domain/Pagamento.java
+++ src/main/java/com/educandoweb/cursomc/domain/Pagamento.java
@@ -11,6 +11,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.educandoweb.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED) // InheritanceType.JOINED cria apenas uma tabela junto com as classes filhas, InheritanceType.SINGLE_TABLE cria uma tabela para cada classe 
@@ -21,6 +22,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId // Serve para garantir que o id do pagamento será igual ao id do pedido.
